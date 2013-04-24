fe702702b0d4e7306087ae0ff8888935ec7fc198
hdong107 <hdong107@163.com>
2013-04-24 18:16:07 +0800

    huangdong

--- js/event_card_frame.js
+++ js/event_card_frame.js
@@ -1,162 +1,118 @@
-// javascript ~ Just do it.
+var expan_div;
 
-/*模拟数据资源*/
-var sourceArray = new Array(["Dangerous", "source/Michael_Jackson_Dangerous_2.jpg", "source/Dangerous.mp4"], ["Cloud Atlas", "source/cloud-atlas-01.jpg", "source/Dangerous.mp4"]);
-
-var event_card;
-
-/*函数调用*/
-// onDisplay();
-
-/**
- * 呈现
- */
-function onDisplay() {
-	/**EventCard整体*/
-	event_card = document.createElement("div");
-	event_card.className = "event_card";
-	document.getElementsByClassName("main")[0].appendChild(event_card);
-
-	// document.activeElement.className = "event_card";
-	console.log("document.activeElement.className = " + document.activeElement.className);
-
-	/**EventCard title部分*/
-	var top_card = document.createElement("div");
-	top_card.className = "top_card";
-	event_card.appendChild(top_card);
-
-	/**EventCard 取景器部分*/
-	var bottom_card = document.createElement("div");
-	bottom_card.className = "bottom_card";
-	event_card.appendChild(bottom_card);
-	this.onAssemble(1);
-
-	/*拓展功能栏*/
-	var expan_div = document.createElement("div");
-	expan_div.className = "expan_div";
-	// event_card.appendChild(expan_div);
-	/**放在第一个子节点之前才有效果*/
-	event_card.insertBefore(expan_div, event_card.childNodes[0]);
-
-	/*功能 1*/
-	var expan_fun1 = document.createElement("div");
-	expan_fun1.className = "expan_fun";
-	expan_fun1.name = "expan_fun1";
-	expan_div.appendChild(expan_fun1);
-
-	/*功能 2*/
-	var expan_fun2 = document.createElement("div");
-	expan_fun2.className = "expan_fun";
-	expan_fun2.name = "expan_fun2";
-	// expan_fun2.style.backgroundColor = "#666665";
-	expan_div.appendChild(expan_fun2);
-
-	/*功能 3*/
-	var expan_fun3 = document.createElement("div");
-	expan_fun3.className = "expan_fun";
-	expan_fun3.name = "expan_fun3";
-	expan_div.appendChild(expan_fun3);
-
-	/*根据b的值,呼出或退出拓展功能栏*/
-	/*
-	 var a = 0;
-
-	 this.event_card.onclick = function show() {
-	 console.log("onclick");
-	 var b = 0;
-	 if(a) {
-	 b = 1;
-	 a = 0;
-	 document.activeElement.name = "expan_fun2";
-	 getFocus(expan_fun2);
-	 } else {
-	 b = 0;
-	 a = 1;
-	 }
-	 display_expanColumn(b);
-	 }
-
-	 this.event_card.onkeydown = function KeyDownDown() {
-	 alert("onkeydown");
-	 if(window.event.keyCode == 39) {
-	 document.activeElement.name = "expan_fun1";
-	 getFocus(expan_fun1);
-	 a = 1;
-	 }
-	 if(window.event.keyCode == 37) {
-	 document.activeElement.className = "event_card";
-	 a = 0;
-	 }
-	 display_expanColumn(a);
-	 }
-	 */
-
+/*------------------创建EventCardDemo类----------------------*/
+EventCardDemo = function() {
+	console.debug("EventCardDemo构造函数");
+	this.onAssemble = EventCardDemo.onAssemble;
 }
-
-/**
- * 呼出及退出拓展功能栏
- */
-function display_expanColumn(b) {
-	var expan_div = document.getElementsByClassName("expan_div")[0];
-	if(b) {
-		console.log("display_expanColumn	true");
-		expan_div.style.display = "block";
-	} else {
-		console.log("display_expanColumn	false");
-		expan_div.style.display = "none";
-	}
-}
-
-/**
- * 拓展功能栏获取焦点的处理
- */
-function getFocus(a) {
-	console.log("document.activeElement.className = " + document.activeElement.className);
-	/*注意是"=="而不是"=" */
-	var b = (document.activeElement.name == a.name);
-	// alert(document.activeElement.name + ", " + a.name + ", " + "b = " + b);
-	if(b) {
-		a.style.backgroundColor = "#666665";
-	}
-}
-
-/**
- * 响应键盘方向右键：呼出拓展功能栏
- * 响应键盘方向左键：退出拓展功能栏
- */
-function keydown() {
-	if(window.event.keyCode == 39) {
-		/* 呼出拓展功能栏*/
-		console.log("——>");
-		display_expanColumn(1);
-	}
-	if(window.event.keyCode == 37) {
-		/* 退出拓展功能栏*/
-		console.log("<——");
-		display_expanColumn(0);
-	}
-}
-
 /**
  * 生成EventCard(组装)
  */
-function onAssemble(id) {
+EventCardDemo.onAssemble = function(id) {
+	console.debug("onAssemble");
+	/*模拟数据资源*/
+	var sourceArray = new Array(["Dangerous", "source/Michael_Jackson_Dangerous_2.jpg", "source/Dangerous.mp4"], ["Cloud Atlas", "source/cloud-atlas-01.jpg", "source/Dangerous.mp4"]);
+
 	document.getElementsByClassName("top_card")[0].innerHTML = sourceArray[id][0];
 
 	var img = document.createElement("img");
 	document.getElementsByClassName("bottom_card")[0].appendChild(img);
 	img.setAttribute("src", sourceArray[id][1]);
 
-	var img_width = this.getOriginalSize(sourceArray[id][1])[0];
-	var img_height = this.getOriginalSize(sourceArray[id][1])[1];
+	/*图片加载完成再执行里面的方法*/
+	img.onload = function() {
+		var img_width = getOriginalSize(sourceArray[id][1])[0];
+		var img_height = getOriginalSize(sourceArray[id][1])[1];
 
-	var img_width_new = this.imageCompress(img_width, img_height)[0];
-	var img_height_new = this.imageCompress(img_width, img_height)[1];
+		console.debug(img_width + ", " + img_height);
 
-	img.setAttribute("width", img_width_new);
-	img.setAttribute("height", img_height_new);
+		var img_width_new = imageCompress(img_width, img_height)[0];
+		var img_height_new = imageCompress(img_width, img_height)[1];
 
-	this.set_img_center(img, img_width_new, img_height_new);
+		img.setAttribute("width", img_width_new);
+		img.setAttribute("height", img_height_new);
+
+		set_img_center(img, img_width_new, img_height_new);
+	}
+}
+/**
+ * 呼出及退出拓展功能栏
+ */
+function display_expanColumn(b) {
+	if(b) {
+		expan_div.style.display = "block";
+	} else {
+		expan_div.style.display = "none";
+	}
+}
+
+/*拓展功能栏焦点变化的标识*/
+var flag = 1;
+
+/*拓展功能栏上功能的个数，现初始值设为3*/
+var fun_count = 3;
+
+/**
+ * 响应键盘方向键事件。
+ * 右：呼出拓展功能栏
+ * 左：退出拓展功能栏
+ * 在拓展功能栏显示状态下：上下方向键切换焦点
+ * 在拓展功能栏隐藏状态下：上下方向键不做任何处理
+ */
+function keydown() {
+	// console.debug("keyCode = " + window.event.keyCode);
+	expan_div = document.getElementsByClassName("expan_div")[0];
+
+	if(window.event.keyCode == 39) {
+		/* 呼出拓展功能栏*/
+		display_expanColumn(1);
+	}
+
+	if(window.event.keyCode == 37) {
+		/* 退出拓展功能栏*/
+		display_expanColumn(0);
+
+		/*焦点回到第一个*/
+		flag = 1;
+	}
+
+	if(expan_div.style.display == "block") {
+		/*上方向键*/
+		if(window.event.keyCode == 38) {
+			flag--;
+			flag = flag >= 1 ? flag : 1;
+		}
+
+		/*下方向键*/
+		if(window.event.keyCode == 40) {
+			flag++;
+			flag = flag <= fun_count ? flag : fun_count;
+		}
+		this.onlyOneFocus(flag);
+	}
+
+	/*在拓展功能不显示的状态下，按enter键播放视频*/
+	if(expan_div.style.display != "block") {
+		if(window.event.keyCode == 13) {
+			this.onExecute();
+		}
+	}
+
+	/*按小键盘的"-"键执行EventCard的销毁*/
+	if(window.event.keyCode == 109) {
+		this.onDestroy();
+	}
+}
+
+/**
+ * 设置拓展功能栏上有且只有一个获取焦点
+ */
+function onlyOneFocus(a) {
+	for(var i = 1; i <= fun_count; i++) {
+		console.log(i);
+		document.getElementsByName("expan_fun"+i)[0].className = "expan_fun";
+	}
+	document.getElementsByName("expan_fun"+a)[0].className = "expan_fun_onfocus";
 }
 
 /**
@@ -172,6 +128,7 @@ function getOriginalSize(path) {
  * 图片压缩,等比例压缩图片的宽高，使其短的一方与取景器对应的宽高保持一致，另一方取中间部分,返回压缩后图片的宽高
  */
 function imageCompress(width_origin, height_origin) {
+	console.debug("width_origin " + width_origin + ", " + "height_origin " + height_origin);
 	if(width_origin >= 500 && height_origin >= 256) {
 		var width_compress_rate = 500 / width_origin;
 		var height_compress_rate = 256 / height_origin;
@@ -190,19 +147,27 @@ function set_img_center(image, width, height) {
 	image.style.left = "-" + width_offset + "px";
 }
 
+/*var int=self.setInterval("onRecognize()",2000);*/
+
 /**
  * 识别
  */
 function onRecognize() {
 	/*识别资源类型*/
-
+	var path = "source/";
+	var file_name;
+	var point = file_name.lastIndexOf(".");
+	var file_type = file_name.subString(point + 1, file_name.length).toLowerCase();
+	if(file_type == "jpg" || file_type == "mp4" || file_type == "mp3") {
+		alert("发现资源");
+	}
 }
 
 /**
  * 执行
  */
 function onExecute() {
-	/*执行EventCard的功能*/
+	window.location = "video.html";
 }
 
 /**
@@ -210,5 +175,7 @@ function onExecute() {
  */
 function onDestroy() {
 	/*EventCard的销毁*/
+	var event_card = document.getElementById("event_card1");
+	event_card.parentNode.removeChild(event_card);
 }
 
